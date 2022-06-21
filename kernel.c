494b706d8db0b2489e50c15fda8e0fcf48970899
Tyler Smith <tjismith13@icloud.com>
2022-06-21 13:14:59 -0400

    Beginning of screen art

--- kernel.c
+++ kernel.c
@@ -5,12 +5,32 @@ void main() {
     uart_init();
     fb_init();
 
-    drawRect(150, 150, 400, 400, 0x03, 0);
-    drawCircle(960, 540, 250, 0x2e, 1);
+    // Arrow 1
+    drawRect(0,     0,    500,   300,   0xFF, 1);
+    drawRect(250,   300,  750,   600,   0xFF, 1);
+    drawRect(500,   600, 1000,  900,  0xFF, 1);
+    drawRect(250,   900, 750,   1200,   0xFF, 1);
+    drawRect(0,     1200, 500,   1500,   0xFF, 1);
 
-    drawString(100, 100, "Hello World", 0x0f);
+    // Arrow 2
+    drawRect(1000,     0,    1500,   300,   0xFF, 1);
+    drawRect(1250,   300,  1750,   600,   0xFF, 1);
+    drawRect(1500,   600, 2000,  900,  0xFF, 1);
+    drawRect(1250,   900, 1750,   1200,   0xFF, 1);
+    drawRect(1000,     1200, 1500,   1500,   0xFF, 1);
 
-    drawLine(100, 500, 350, 700, 0x0c);
+    drawRect(2500, 0, 2750, 1750, 0xFF, 1);
+    drawRect(2000, 1000, 2500, 1250, 0xFF, 1);
+    drawRect(2000, 0, 2250, 1000, 0xFF, 1);
+
+
+   
+   
+    // drawCircle(960, 540, 250, 0x2e, 1);
+
+     
+
+    // drawLine(100, 500, 350, 700, 0x0c);
 
     while(1);
 }
--- terminal.h
+++ terminal.h
@@ -18,8 +18,8 @@ unsigned int vgapal[] = {
 };
 
 enum {
-    FONT_WIDTH     = 8,
-    FONT_HEIGHT    = 8,
+    FONT_WIDTH     = 100,
+    FONT_HEIGHT    = 100,
     FONT_BPG       = 8,  // Bytes per glyph
     FONT_BPL       = 1,  // Bytes per line
     FONT_NUMGLYPHS = 224
